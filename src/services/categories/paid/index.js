fd345a7ef7694017ec65130fae9a0b95c0bb2885
lucaperullo <lucaperullo@outlook.it>
2022-09-10 13:53:19 +0200

    logout fix

---
+++ src/services/categories/paid/index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import { authorize } from "../../auth/middleware.js";
+import paidsSchema from "./schema.js";
+
+const paidsRoutes = express.Router();
+
+const errorHandler = async (errorText, value, httpStatusCode) => {
+  const err = new Error();
+  err.errors = [{ value: value, msg: errorText }];
+  err.httpStatusCode = httpStatusCode || 400;
+  return err;
+};
+
+paidsRoutes.post("/", authorize, async (req, res, next) => {
+  try {
+    const newpaid = new paidsSchema(req.body);
+    newpaid.save();
+    res.status(201).send(newpaid);
+  } catch (error) {
+    next(await errorHandler(error));
+  }
+});
+
+paidsRoutes.get("/", async (req, res, next) => {
+  try {
+    // populate the categories with the pages
+    let populatedCategories = await paidsSchema.find().populate("paids");
+
+    res.send(populatedCategories);
+  } catch (error) {
+    console.log(error);
+    res.send({ message: error.message });
+    next(await errorHandler(error));
+  }
+});
+
+paidsRoutes.get("/:id", async (req, res, next) => {
+  try {
+    const paid = await paidsSchema.findById(req.params.id).populate("paids");
+    res.send(paid);
+  } catch (error) {
+    next(await errorHandler(error));
+  }
+});
+
+paidsRoutes.put("/:id", authorize, async (req, res, next) => {
+  try {
+    const paid = await paidsSchema.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.send(paid);
+  } catch (error) {
+    next(await errorHandler(error));
+  }
+});
+
+paidsRoutes.delete("/:id", authorize, async (req, res, next) => {
+  try {
+    const paid = await paidsSchema.findByIdAndDelete(req.params.id);
+    res.send(paid);
+  } catch (error) {
+    next(await errorHandler(error));
+  }
+});
+
+export default paidsRoutes;
